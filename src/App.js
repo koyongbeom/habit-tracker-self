77cad03ceb82afbd784324dcab37f3ece302024b
koyongbeom <koyongbeom@gmail.com>
2021-01-09 15:33:28 +0900

    Fourth commit - header design add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { faRunning } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <div>
+      <div class="header">
+        <i class="fas fa-leaf fa-2x"></i>
+        <span class="header-text">Habit Tracker</span>
+        <span class="header-count">0</span>
+      </div>
       <Habits></Habits>
     </div>
   );
